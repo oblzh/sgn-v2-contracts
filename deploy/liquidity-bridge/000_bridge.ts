0dad8813f0d70df8e1fb3cc8c0c6f6fa0a979cfb
liuxiaobleach <1241368737@qq.com>
2023-04-03 15:54:55 +0800

    add new zksync

--- deploy/liquidity-bridge/000_bridge.ts
+++ deploy/liquidity-bridge/000_bridge.ts
@@ -13,7 +13,7 @@ const deployFunc = async (hre: HardhatRuntimeEnvironment) => {
 
   // Create deployer object and load the artifact of the contract we want to deploy.
   const deployer = new Deployer(hre, wallet);
-  const artifact = await deployer.loadArtifact('Bridge');
+  const artifact = await deployer.loadArtifact('MessageBus');
 
   // Estimate contract deployment fee
   // const deploymentFee = await deployer.estimateDeployFee(artifact, []);
@@ -30,11 +30,28 @@ const deployFunc = async (hre: HardhatRuntimeEnvironment) => {
   //const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
   //console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
 
+  // deploy pegbridgeV2
+  //const args = [process.env.PEGGED_TOKEN_BRIDGE_SIGS_VERIFIER];
+  //const pegV2Contract = await deployer.deploy(artifact, args);
+
+  // deploy message bus
+  const args = [
+    process.env.MESSAGE_BUS_SIGS_VERIFIER,
+    process.env.MESSAGE_BUS_LIQUIDITY_BRIDGE as string,
+    process.env.MESSAGE_BUS_PEG_BRIDGE as string,
+    process.env.MESSAGE_BUS_PEG_VAULT as string,
+    process.env.MESSAGE_BUS_PEG_BRIDGE_V2 as string,
+    process.env.MESSAGE_BUS_PEG_VAULT_V2 as string
+  ];
+  const messageBusContract = await deployer.deploy(artifact, args);
+
+
+  // deploy bridge
   // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
-  const bridgeContract = await deployer.deploy(artifact, []);
+  //const bridgeContract = await deployer.deploy(artifact, []);
 
   // Show the contract info.
-  const contractAddress = bridgeContract.address;
+  const contractAddress = messageBusContract.address;
   console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
 };
 
